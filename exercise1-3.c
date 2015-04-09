a27e964ba90958cddca244c17fe8bebf6a5a6896
Guilherme Rizzo <guivr2011@gmail.com>
2015-04-09 15:05:56 -0300

    Added function to Exercise 1.3

--- exercise1-3.c
+++ exercise1-3.c
@@ -2,26 +2,33 @@
 #include <stdlib.h>
 #define SIZE 10
 
+int function();
+int array[SIZE];
+int i = 0;
+int last_char = 0;
+int stop = 0;
+
 int main()
 {
-    int array[SIZE];
-    int last_char, i, stop;
-    i = last_char = stop = 0;
     while(stop != 1){
-        array[last_char] = getchar();
-        if(last_char == '\n'){
-            stop = 1;
-        }
-        else{
-            last_char+=1;
-            if(last_char == SIZE){
-                stop = 1;
-            }
-        }
+        function();
     }
     while(i < last_char){
         printf("%c", array[i]);
         i++;
     }
     return 0;
+}
+
+function(){
+    array[last_char] = getchar();
+    if(last_char == '\n'){
+        stop = 1;
+    }
+    else{
+        last_char+=1;
+        if(last_char == SIZE){
+            stop = 1;
+        }
+    }
 }
